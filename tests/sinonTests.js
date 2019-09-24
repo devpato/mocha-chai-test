8a9cdae5111fc18cc51bfa043716b81025c8828c
Patricio Vargas <pevargas2730@gmail.com>
2019-09-24 13:28:09 -0400

    one more test to check if its object or callback

--- tests/sinonTests.js
+++ tests/sinonTests.js
@@ -6,30 +6,45 @@ chai.should();
 
 describe('sinon tests', () => {
   let student;
+  let schedule;
 
   beforeEach(() => {
     student = {
       dropClass: (classId, cb) => {
-        //do stuff
-        cb();
+        if (!!cb.dropClass) {
+          cb.dropClass();
+        } else {
+          cb();
+        }
+      }
+    };
+
+    schedule = {
+      dropClass: () => {
+        console.log('Class dropped');
       }
     };
   });
   describe('student.dropClass', () => {
-    it('should call callback', () => {
-      let called = false;
-      const callback = () => {
-        called = true;
+    it('should call callback and log the console', () => {
+      const onClassDropped = () => {
+        console.log('my class dropped');
       };
-
-      student.dropClass(1, callback);
-      expect(called).to.be.true;
+      const spy = sinon.spy(onClassDropped);
+      student.dropClass(1, spy);
+      spy.called.should.be.true;
     });
 
     it('spy on callback', () => {
       let spy = sinon.spy();
       student.dropClass(1, spy);
       spy.called.should.be.true;
+    });
+
+    it('spy on callback even if its a method or object', () => {
+      sinon.spy(schedule, 'dropClass');
+      student.dropClass(1, schedule);
+      schedule.dropClass.called.should.be.true;
     });
   });
 });
