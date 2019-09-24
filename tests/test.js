b6a43613231edfc4be3e1d815c47d9a794b1194c
Patricio Vargas <pevargas2730@gmail.com>
2019-09-24 11:03:56 -0400

    is Even test

--- tests/test.js
+++ tests/test.js
@@ -2,11 +2,15 @@
 
 const isEven = require('../src/isEven');
 const chai = require('chai');
-
+const expect = chai.expect;
 chai.should();
 
 describe('isEven', () => {
   it('should return true when a number is even', () => {
     isEven(4).should.be.true;
+  });
+
+  it('should return false when a number is odd', () => {
+    expect(isEven(5).should.be.false);
   });
 });
