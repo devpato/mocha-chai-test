dffe0e90e92015c1c9c67fbd9dee9da5696fc70d
Patricio Vargas <pevargas2730@gmail.com>
2019-09-24 11:12:19 -0400

    grouped tests

--- tests/test.js
+++ tests/test.js
@@ -6,29 +6,31 @@ const chai = require('chai');
 const expect = chai.expect;
 chai.should();
 
-describe('isEven', () => {
-  it('should return true when a number is even', () => {
-    isEven(4).should.be.true;
+describe('number tests', () => {
+  describe('isEven', () => {
+    it('should return true when a number is even', () => {
+      isEven(4).should.be.true;
+    });
+
+    it('should return false when a number is odd', () => {
+      expect(isEven(5).should.be.false);
+    });
   });
 
-  it('should return false when a number is odd', () => {
-    expect(isEven(5).should.be.false);
-  });
-});
+  describe('add without step teardown', () => {
+    let num;
 
-describe('add without step teardown', () => {
-  let num;
+    beforeEach(() => {
+      num = 5;
+    });
 
-  beforeEach(() => {
-    num = 5;
-  });
+    it('should return true when a number is even', () => {
+      num = add(num, 5);
+      num.should.equal(10);
+    });
 
-  it('should return true when a number is even', () => {
-    num = add(num, 5);
-    num.should.equal(10);
-  });
-
-  it('should return true when a number is even', () => {
-    add(num, 7).should.equal(12);
+    it('should return true when a number is even', () => {
+      add(num, 7).should.equal(12);
+    });
   });
 });
