30cb5763b73326a87cd8f8b5ce52756b3dcd7393
Patricio Vargas <pevargas2730@gmail.com>
2019-09-24 11:10:24 -0400

    add before each and more test

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const isEven = require('../src/isEven');
+const add = require('../src/add');
 const chai = require('chai');
 const expect = chai.expect;
 chai.should();
@@ -12,5 +13,22 @@ describe('isEven', () => {
 
   it('should return false when a number is odd', () => {
     expect(isEven(5).should.be.false);
+  });
+});
+
+describe('add without step teardown', () => {
+  let num;
+
+  beforeEach(() => {
+    num = 5;
+  });
+
+  it('should return true when a number is even', () => {
+    num = add(num, 5);
+    num.should.equal(10);
+  });
+
+  it('should return true when a number is even', () => {
+    add(num, 7).should.equal(12);
   });
 });
